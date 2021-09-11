ee34b36dec6c6ee873703b8b34998b1d25919f6e
erin-switchstitch <83361880+erin-switchstitch@users.noreply.github.com>
2021-09-11 15:57:59 +0100

    V.1.0 Group work in class
    Basic html written and JS added. Some functionality needs adding and styling

--- challenge-fastest-presser/script.js
+++ challenge-fastest-presser/script.js
@@ -1,11 +1,59 @@
-function startGame() {}
+const setTime = document.getElementById("setTime");
+const startBtn = document.getElementById("startGameBtn");
+const leftCount = document.getElementById("leftCount");
+const rightCount = document.getElementById("rightCount");
+console.log(startBtn);
+let time;
+let timer;
+let sCounter = 0;
+let lCounter = 0;
+
+
+function startGame() {
+  // sCounter = 0;
+  // lCounter = 0;
+
+  time = Number(setTime.value);
+  timer = setInterval(function(){
+    
+    console.log(time)
+    time--;
+
+    if(time === 0){
+      clearInterval(timer);
+      console.log("sCounter : " + sCounter)
+      console.log("lCounter : " + lCounter)
+      console.log("TIME ENDED")
+    } else if (time <= 0) {
+      clearInterval(timer);
+      alert("Incorrect time input")
+    }
+
+  }, 1000);
+  
+  
+}
 
 function keyBoardEvents(e) {
-  if (e.keyCode === 83) {
-    // On 'S' Pressed
-  } else if (e.keyCode === 76) {
-    // On 'L' Pressed
+  console.log(e.keyCode);
+
+  if (time > 0) {
+      if ((e.keyCode === 83) || (e.keyCode === 115))  {
+        // On 'S' Pressed
+        sCounter ++;
+        //console.log("sCounter : " + sCounter)
+        leftCount.innerText = sCounter;
+      } else if ((e.keyCode === 76) || (e.keyCode === 108))  {
+        // On 'L' Pressed
+        lCounter ++;
+        //console.log("lCounter : " + lCounter)
+        rightCount.innerText = lCounter;
+      }
   }
 }
 
 document.addEventListener("keypress", keyBoardEvents);
+// document.removeEventListener
+ startBtn.addEventListener("click", ()=>{
+   startGame();
+})
